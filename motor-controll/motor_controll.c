eecedc8c4fb5bf7eeeadfcc491dfb43728104bf5
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2018-10-30 12:02:12 +0100

    Motor is now controlled using two seperate PWMs. Only two H-Bridges are used by one ATtiny. Pin3 can now be used to select either I²C Address 0x01 or 0x02.

--- motor-controll/motor_controll.c
+++ motor-controll/motor_controll.c
@@ -5,50 +5,52 @@
 
 void initializeI2C()
 {
-    usiTwiSlaveInit(0x01);
+    DDRB &= ~(1 << PIN3);
+    if (PINB & (1 << PIN3))
+    {
+        usiTwiSlaveInit(0x01);
+    }
+    else
+    {
+        usiTwiSlaveInit(0x02);
+    }
     sei();
 }
 
 void initializePWM()
 {
-    // Timer1
-    TCCR1 = (1 << CS10); // no prescaler
-    GTCCR = (1 << COM1B1) | (1 << PWM1B); // clear OC1B on compare
-    OCR1B = 0; // initial duty cycle
-    OCR1C = 127; // frequency
-    
-    DDRB |= (1 << PIN4); // OC1B as Output for PWM1
-}
+     // Timer 0
+    TCCR0A = (1<<WGM00) | (1<<WGM01) | (1<<COM0A1) | (1<<COM0B1) | (1<<COM0A0) | (1<<COM0B0); // fast PWM, set OC0A and OC0B on compare
+    TCCR0B = (1<<CS00); // fast PWM, top at 0xFF, no prescaler
+    OCR0A = OCR0B = 0; // duty cycle // pulse width
 
-void initializeDirectionSelect()
-{
-    DDRB |= (1 << PIN1) | (1 << PIN3);
-    PORTB &= ~((1<< PIN1) | (1 << PIN3));
+    // Timer 1
+    TCCR1 = (1<<CS10); // no prescaler
+    GTCCR = (1<<COM1B1) | (1<<PWM1B); // clear OC1B on compare
+    OCR1B = 0; // duty cycle // pulse width
+    OCR1C = 255; // frequency
+
+    // Set PWM Pins as Output
+    DDRB |= (1 << PIN4) | (1 << PIN1);
 }
 
 void initialize()
 {
     initializeI2C();
     initializePWM();
-    initializeDirectionSelect();
 }
 
-void setMotorSpeed(uint8_t command)
-{
-    OCR1B = command & 0x7F;
-}
-
-void setMotorDirection(uint8_t command)
+void controllMotor(uint8_t command)
 {
     if (command & 0x80)
     {
-        PORTB &= ~(1 << PIN3);
-        PORTB |= (1 << PIN1);
+        OCR1B = 0;
+        OCR0B = 0xFF - (command & 0x7F) * 2;
     }
     else
     {
-        PORTB &= ~(1 << PIN1);
-        PORTB |= (1 << PIN3);
+        OCR0B = 0xFF;
+        OCR1B = (command & 0x7F) * 2;
     }
 }
 
@@ -56,11 +58,8 @@ int main()
 {
     initialize();
 
-    uint8_t command= 0;
     while (1)
     {
-        command = usiTwiReceiveByte();
-        setMotorSpeed(command);
-        setMotorDirection(command);
+        controllMotor(usiTwiReceiveByte());
     }
 }
