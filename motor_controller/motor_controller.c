2f6ce0c50eb3dacfa2c7f489f80f419468850886
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2019-01-08 09:53:11 +0100

    Now only uses one PWM.

--- motor_controller/motor_controller.c
+++ motor_controller/motor_controller.c
@@ -3,54 +3,53 @@
 
 #include "usi_twi_slave.h"
 
+#ifndef address
+#   define address 0x01
+#endif
+
 void initializeI2C()
 {
-    DDRB &= ~(1 << PIN3);
-    if (PINB & (1 << PIN3))
-    {
-        usiTwiSlaveInit(0x01);
-    }
-    else
-    {
-        usiTwiSlaveInit(0x02);
-    }
+    usiTwiSlaveInit(address);
     sei();
 }
 
 void initializePWM()
 {
-     // Timer 0
-    TCCR0A = (1<<WGM00) | (1<<WGM01) | (1<<COM0A1) | (1<<COM0B1) | (1<<COM0A0) | (1<<COM0B0); // fast PWM, set OC0A and OC0B on compare
-    TCCR0B = (1<<CS00); // fast PWM, top at 0xFF, no prescaler
-    OCR0A = OCR0B = 0xFF; // duty cycle // pulse width
-
     // Timer 1
-    TCCR1 = (1<<CS10); // no prescaler
-    GTCCR = (1<<COM1B1) | (1<<PWM1B); // clear OC1B on compare
+    TCCR1 = (1 << CS10); // no prescaler
+    GTCCR = (1 << COM1B1) | (1 << PWM1B); // clear OC1B on compare
     OCR1B = 0; // duty cycle // pulse width
-    OCR1C = 255; // frequency
+    OCR1C = 127; // frequency
 
-    // Set PWM Pins as Output
-    DDRB |= (1 << PIN4) | (1 << PIN1);
+    // Set PWM Pin as Output
+    DDRB |= (1 << PIN4);
+}
+
+void initializeDirectionPins()
+{
+    DDRB |= (1 << PIN1) | (1 << PIN3);
+    PORTB &= ~((1 << PIN1) | (1 << PIN3));
 }
 
 void initialize()
 {
     initializeI2C();
     initializePWM();
+    initializeDirectionPins();
 }
 
 void controlMotor(uint8_t command)
 {
+    OCR1B = command & 0x7F;
     if (command & 0x80)
     {
-        OCR1B = 0;
-        OCR0B = 0xFF - (command & 0x7F) * 2;
+        PORTB &= ~(1 << PIN1);
+        PORTB |= (1 << PIN3);
     }
     else
     {
-        OCR0B = 0xFF;
-        OCR1B = (command & 0x7F) * 2;
+        PORTB &= ~(1 << PIN3);
+        PORTB |= (1 << PIN1);
     }
 }
 
